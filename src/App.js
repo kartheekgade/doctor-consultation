170affba7b2746d97db3ad5ca50943557a8d5360
kartheek <krthkgade@gmail.com>
2023-11-26 11:08:46 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ReactMic } from 'react-mic';
 
 function App() {
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioBlob, setAudioBlob] = useState(null);
+
+  const startRecording = () => {
+    setIsRecording(true);
+  };
+
+  const stopRecording = () => {
+    setIsRecording(false);
+  };
+
+  const onData = (recordedData) => {
+    // console.log('chunk of real-time data is: ', recordedData);
+  };
+
+  const onStop = (recordedBlob) => {
+    setAudioBlob(recordedBlob);
+  };
+
+  const handleUpload = () => {
+    // Handle the audio upload logic here
+    if (audioBlob) {
+      console.log('Uploading audio blob:', audioBlob);
+      // Add your upload logic here, e.g., send the audioBlob to a server
+    } else {
+      console.log('No audio to upload.');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Doctor Consultation</h1>
+      <div>
+        <h2>Audio Recording</h2>
+        <button onClick={startRecording} disabled={isRecording}>
+          Start Recording
+        </button>
+        <button onClick={stopRecording} disabled={!isRecording}>
+          Stop Recording
+        </button>
+        <ReactMic
+          record={isRecording}
+          className="sound-wave"
+          onStop={onStop}
+          onData={onData}
+          strokeColor="#000000"
+          backgroundColor="#FF4081"
+        />
+      </div>
+      <div>
+        <h2>Audio Upload</h2>
+        <input
+          type="file"
+          accept=".mp3"
+          onChange={(e) => setAudioBlob(e.target.files[0])}
+        />
+        <button onClick={handleUpload}>Upload Audio</button>
+      </div>
     </div>
   );
 }
